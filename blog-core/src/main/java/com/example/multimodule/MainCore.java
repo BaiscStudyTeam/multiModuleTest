fd30c3beb1097409fe0f7d68162c3e8c224d0992
leeyoosong <zaxscd95@naver.com>
2023-05-20 12:49:20 +0900

    UPDATE MainCore @Autowired injection to private Constructor Injection

--- blog-core/src/main/java/com/example/multimodule/MainCore.java
+++ blog-core/src/main/java/com/example/multimodule/MainCore.java
@@ -1,19 +1,19 @@
 package com.example.multimodule;
 
 import com.example.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.login.service.LoginService;
 
 @RestController
+@RequiredArgsConstructor
 public class MainCore {
 
-    @Autowired
-    public BoardService boardService;
+    private final BoardService boardService;
 
-    @Autowired
-    public LoginService loginService;
+    private final LoginService loginService;
 
 
     @GetMapping("/")
