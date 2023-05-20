0bc8e0d3312f845b369aab3e50fc4d6af2a24401
yoosonglee <zaxscd95@naver.com>
2023-05-20 19:48:41 +0900

    UPDATE Sentry Test Code

--- blog-core/src/main/java/com/example/multimodule/controller/LoginController.java
+++ blog-core/src/main/java/com/example/multimodule/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.multimodule.controller;
 
 import com.example.login.service.LoginService;
+import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,5 +20,19 @@ public class LoginController {
     public ResponseEntity<Object> insertUser() {
         loginService.insertLoginUser();
         return ResponseEntity.ok().body("SUCCESS");
+    }
+
+    // PULLREQUEST용 주석 추가
+    @GetMapping("/testSentry")
+    public ResponseEntity<Object> testSentry() {
+        Sentry.init(options -> {
+            options.setDsn("https://aca63734d7b844709390e4b636d2a967@o4505215665897472.ingest.sentry.io/4505215667011584");
+        });
+        try {
+            throw new Exception("SPRING BOOT : Throw Test Exception");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
+        return ResponseEntity.ok().body(null);
     }
 }
