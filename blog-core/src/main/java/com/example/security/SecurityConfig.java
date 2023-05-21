52b7e49852dea62d774097dd843ec905a7d6c217
yoosonglee <zaxscd95@naver.com>
2023-05-21 11:03:26 +0900

    UPDATE Spring Security Config, Disable Default Authentication

---
+++ blog-core/src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Spring Security LoginPage disable
+        http.cors().and().csrf().disable().formLogin().disable();
+        return http.build();
+    }
+}
